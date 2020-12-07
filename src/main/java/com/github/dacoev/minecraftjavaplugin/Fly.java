7cf9e15471088aca1e849274c92ed6fad62be051
Housen Yim <justin.yim14@gmail.com>
2020-12-07 14:10:47 +0900

    Initial commit

---
+++ src/main/java/com/github/dacoev/minecraftjavaplugin/Fly.java
@@ -0,0 +1,13 @@
+package com.github.dacoev.minecraftjavaplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Fly implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return false;
+    }
+}
