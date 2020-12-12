02dcba5a5a3c12f9b554edbc536ccd91473c10c2
Housen Yim <justin.yim14@gmail.com>
2020-12-12 11:55:18 +0900

    Finish

--- src/main/java/com/github/dacoev/minecraftjavaplugin/Heal.java
+++ src/main/java/com/github/dacoev/minecraftjavaplugin/Heal.java
@@ -5,6 +5,7 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -27,10 +28,18 @@ public class Heal implements CommandExecutor {
                 TextComponent message = new TextComponent("Would you like to be healed?");
                 message.setColor(ChatColor.GOLD);
                 message.setBold(true);
+                //어렵다...
                 message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/doctor healme"));
-                message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click here to be healed!").color(ChatColor.GRAY).italic(true).create())));
+                message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click here to be healed!").color(ChatColor.GRAY).italic(true).create()));
+                player.spigot().sendMessage(message);
             }
-            //
+            if (args[0].equalsIgnoreCase("healme")){
+                player.setHealth(20.0);
+                //Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "heal " + player.getName());
+                player.sendMessage(ChatColor.GREEN + "You've been healed!");
+                return true;
+            }
+            player.sendMessage(ChatColor.RED + "Usage: /doctor");
         }
         return false;
     }
