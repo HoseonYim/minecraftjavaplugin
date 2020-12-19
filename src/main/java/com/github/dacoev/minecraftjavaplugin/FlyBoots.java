397b9011bbe802e5137c592284d181f05baf66ca
Housen Yim <justin.yim14@gmail.com>
2020-12-19 19:40:03 +0900

    Initial commit

--- src/main/java/com/github/dacoev/minecraftjavaplugin/FlyBoots.java
+++ src/main/java/com/github/dacoev/minecraftjavaplugin/FlyBoots.java
@@ -5,9 +5,14 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FlyBoots implements CommandExecutor {
 
@@ -19,6 +24,10 @@ public class FlyBoots implements CommandExecutor {
                 sender.sendMessage("Sorry console you're not allowed to use this command");
             }
             Player player = (Player) sender;
+
+            player.getInventory().addItem(getItem());
+            player.sendMessage(ChatColor.GOLD + "The Minecraft Legends gave you a gift");
+            return true;
         }
         return false;
     }
@@ -28,8 +37,18 @@ public class FlyBoots implements CommandExecutor {
         ItemMeta meta = boots.getItemMeta();
 
         meta.setDisplayName(ChatColor.GREEN + "" + ChatColor.BOLD + "Boots of Leaping");
-        
+        List<String> lore = new ArrayList<>();
+        lore.add("");
+        lore.add(ChatColor.GOLD + "" + ChatColor.ITALIC + "Boots made for the Minecraft Jumpers");
+        meta.setLore(lore);
 
-        return null;
+        meta.addEnchant(Enchantment.PROTECTION_FALL, 1, true);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        meta.setUnbreakable(true);
+
+        boots.setItemMeta(meta);
+
+        return boots;
     }
 }
